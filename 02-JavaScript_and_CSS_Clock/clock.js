8c35f88b962b45a505d7ae1e133a8caf8765684a
Goncalo Novo <goncalo.novo@protonmail.com>
2017-12-03 13:48:17 +0000

    Finished Challenge 2

--- 02-JavaScript_and_CSS_Clock/clock.js
+++ 02-JavaScript_and_CSS_Clock/clock.js
@@ -1,1 +1,28 @@
-clock.js
+const secondHand = document.querySelector('.second-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360) + 90; /* Add 90 to account for the transform: rotate(90deg) */
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const minutes = now.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360) + ((seconds / 60) * 6) + 90;
+  minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = now.getHours();
+  const hoursDegrees = ((hours / 12) * 360) + ((minutes / 60) * 30) + 90;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  if (secondsDegrees === 90) secondHand.style.transition = 'all 0s';
+  else secondHand.style.transition = 'all 0.05s';
+
+  if (minutesDegrees === 90) minuteHand.style.transition = 'all 0s';
+  else minuteHand.style.transition = 'all 0.1s';
+
+  console.log('It\'s： ' + hours + ":" + minutes + ":" + seconds);
+}
+
+setInterval(setDate, 1000);
