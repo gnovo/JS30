bd60b34adedc9c47c1571400f755898d33ecffb7
Goncalo Novo <goncalo.novo@protonmail.com>
2017-12-12 02:41:58 +0000

    Finished Challenge 17

--- 17-Sorting_Band_Names_without_Articles/sort.js
+++ 17-Sorting_Band_Names_without_Articles/sort.js
@@ -1,1 +1,7 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const strip = (bandName) => bandName.replace(/^(a\s|the\s|an\s)/i, '').trim(); //remove 'a', 'the' and 'an'.
+const sortedBands = bands.sort((a, b) => strip(a) > strip(b) ? 1 : -1); //sort alphabetically comparing the bands without articles
+const render = sortedBands.map(band => `<li>${band}</li>`).join("");
+
+document.querySelector("#bands").insertAdjacentHTML("beforeend", render);
